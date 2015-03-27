f0e4ddd5a678e871b5dff2152c54c743d975f8da
Filipe Regadas <filipe.regadas@outplay.com>
2015-03-27 11:07:29 +0000

    Add missing OpsWorksHelper when searching nodes

--- definitions/mongodb.rb
+++ definitions/mongodb.rb
@@ -214,13 +214,17 @@ define :mongodb_instance,
 
   # replicaset
   if new_resource.is_replicaset && new_resource.auto_configure_replicaset
-    rs_nodes = search(
-      :node,
-      "mongodb_cluster_name:#{new_resource.replicaset['mongodb']['cluster_name']} AND \
-       mongodb_is_replicaset:true AND \
-       mongodb_shard_name:#{new_resource.replicaset['mongodb']['shard_name']} AND \
-       chef_environment:#{new_resource.replicaset.chef_environment}"
-    )
+    if OpsWorksHelper.opsworks?(node)
+      rs_nodes = OpsWorksHelper.replicaset_members(node)
+    else
+      rs_nodes = search(
+        :node,
+        "mongodb_cluster_name:#{new_resource.replicaset['mongodb']['cluster_name']} AND \
+         mongodb_is_replicaset:true AND \
+         mongodb_shard_name:#{new_resource.replicaset['mongodb']['shard_name']} AND \
+         chef_environment:#{new_resource.replicaset.chef_environment}"
+      )
+    end
 
     ruby_block 'config_replicaset' do
       block do
